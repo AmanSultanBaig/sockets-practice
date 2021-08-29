886debf36132813fd692f46d3490fbab373b3571
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-08-29 18:35:40 +0500

    basic server structure is ready

--- servers/index.js
+++ servers/index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// console.log("Welcome to Sockets programming")
+const express = require("express")
+const app = express();
+const cors = require("cors")
+const _http = require("http")
+
+app.use(cors());
+
+const server = _http.createServer(app);
+
+let port = 8080 || process.env.PORT 
+server.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
