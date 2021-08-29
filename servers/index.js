64aa3253716d1605ffb81923c2c82196c7757dfe
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-08-29 18:56:03 +0500

    socket initilzed into a project of backend

--- servers/index.js
+++ servers/index.js
@@ -4,9 +4,23 @@ const app = express();
 const cors = require("cors")
 const _http = require("http")
 
+const { Server } = require("socket.io")
+
 app.use(cors());
 
 const server = _http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
 
-let port = 8080 || process.env.PORT 
+io.on("connection", (socket) => {
+    console.log(socket.id)
+
+    io.on("disconnect", () => console.log(`This socketIs: ${socket.id} user is diconnected!`))
+})
+
+let port = 8080 || process.env.PORT
 server.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
