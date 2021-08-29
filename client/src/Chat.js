77b63d73a4ceae10c40dbd7897e8e15dfa049e80
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-08-29 20:41:18 +0500

    chat data sent to backend

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Chat({socket, username, roomId}) {
-    console.log(socket)
-    console.log(username)
-    console.log(roomId)
+function Chat({ socket, username, roomId }) {
+    const [currentMessage, setCurrentMessage] = useState("")
+
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            let message = {
+                room: roomId,
+                author: username,
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+            await socket.emit("send_message", message)
+        }
+    }
+
     return (
         <div>
             <div className="chat-header">
@@ -11,8 +22,8 @@ function Chat({socket, username, roomId}) {
             </div>
             <div className="chat-body"></div>
             <div className="chat-footer">
-                <input placeholder="Type a message..."/>
-                <button>&#9658;</button>
+                <input placeholder="Type a message..." value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} />
+                <button onClick={sendMessage}>&#9658;</button>
             </div>
         </div>
     )
